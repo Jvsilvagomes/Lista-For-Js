e5b5a26a600667681834ea7d58d9ed95bbe0979f
João Victor <joao.silvassenai@gmail.com>
2025-04-04 11:32:59 -0300

    Contar quantos números divisíveis por 3 existem entre 1 e N (N = 50)

---
+++ exer08.js
@@ -0,0 +1,10 @@
+let quantidade = 0
+for (let i = 1; i <= 50; i++){
+    if(i % 3 == 0){
+        quantidade = quantidade + 1
+    }
+}
+
+    console.log(`A quantidade de divisiveis é de: `+ quantidade)
+
+// joao victor 04/04/2025
