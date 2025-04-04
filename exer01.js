6fdb5a384ba393b3063d92632a923fd4b86a8df3
João Victor <joao.silvassenai@gmail.com>
2025-04-04 09:50:03 -0300

    exer01.js

---
+++ exer01.js
@@ -0,0 +1,5 @@
+for (let i = 1; i <= 50; i++ ){
+    console.log(i)
+}
+
+// joao victor da silva gomes 04/04/2025
---
+++ exer02.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 100; i++){
+    if(i % 2 == 0){
+        console.log(i)
+    }
+}
+
+// joao victor da silva gomes 04/04/2025
